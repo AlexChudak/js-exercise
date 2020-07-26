1a921dae67d907be063e39f3cf978d162cbc0407
Aleksandr Chudak <chudak.alex@gmail.com>
2020-07-26 17:09:01 +0300

    Решил 5 задач 1-го модуля

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const name = '«Генератор защитного поля»'; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log (firstMessage); 
+
+// Write code on this line
+ price = price+1000;
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+// console.log(firstMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+console.log(secondMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
